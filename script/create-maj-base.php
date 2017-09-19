dac775e874b853c8d9d137803e14b37d0d515a81
atm-ph <phf@atm-consulting.Fr>
2017-09-19 10:45:27 +0200

    First Commit

---
+++ script/create-maj-base.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Script créant et vérifiant que les champs requis s'ajoutent bien
+ */
+
+if(!defined('INC_FROM_DOLIBARR')) {
+	define('INC_FROM_CRON_SCRIPT', true);
+
+	require('../config.php');
+
+}
+
+
+/* uncomment
+
+
+dol_include_once('/personalhomepage/class/personalhomepage.class.php');
+
+$PDOdb=new TPDOdb;
+
+$o=new TPersonalHomePage;
+$o->init_db_by_vars($PDOdb);
+
+$o=new TPersonalHomePageChild;
+$o->init_db_by_vars($PDOdb);
+*/
